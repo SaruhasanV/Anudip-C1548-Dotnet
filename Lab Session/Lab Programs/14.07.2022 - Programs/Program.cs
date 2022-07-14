2b5dfdd600f1fb176d53e8646c425977d957f870
SaruhasanV <104126682+SaruhasanV@users.noreply.github.com>
2022-07-14 13:21:23 +0530

    Add files via upload

---
+++ Lab Session/Lab Programs/14.07.2022 - Programs/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Write a Program to perform addition of all elements in Array
+namespace ConsoleApplication7
+{
+    //MAin Class
+    class Program
+    {
+        //Main Method
+        static void Main(string[] args)
+        {
+            //Declaration of array
+            int[] arr = new int[5];
+            int i;
+            int Sum = 0;
+            Console.WriteLine("Addition of all elements in array:");
+            Console.Write("");
+            //Loop for add elements in array and add them
+            for (i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Enter the number");
+                arr[i] = Int32.Parse(Console.ReadLine());
+                Sum = Sum + arr[i];
+            }
+            //Printing Output
+            Console.WriteLine("Addition of all elements stored in the Array:" + Sum);
+            Console.ReadLine();
+        }
+    }
+}
